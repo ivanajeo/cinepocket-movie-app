b1111a748ee4ea1514075f263d675a1277ca6cba
Van <152158486+ivanajeo@users.noreply.github.com>
2025-08-08 05:14:07 +0700

    Implement movie search with debounced input and empty state handling

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -6,9 +6,11 @@ import { Image, TextInput, View } from 'react-native';
 interface Props {
   placeholder: string;
   onPress?: () => void;
+  value: string;
+  onChangeText?: (text: string) => void;
 }
 
-const SearchBar = ({placeholder, onPress}: Props) => {
+const SearchBar = ({placeholder, onPress, value, onChangeText}: Props) => {
   return (
     <View className='flex-row items-center bg-dark-200 rounded-full px-5 py-4'>
       <Image
@@ -19,10 +21,10 @@ const SearchBar = ({placeholder, onPress}: Props) => {
       />
       <TextInput 
         onPress={onPress}
-        onChangeText={() => {}}
+        onChangeText={onChangeText}
         placeholder={placeholder}
         placeholderTextColor='#ab8bff'
-        value=''
+        value={value}
         className='flex-1 ml-2 text-white'
       />
     </View>
